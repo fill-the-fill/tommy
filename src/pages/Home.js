ff31e252e4db89136211265075309c7e04608c8b
fill-the-fill <fill.ickevics@gmail.com>
2022-02-22 12:38:40 +0200

    add carousel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,23 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
-// import Carousel from "react-material-ui-carousel";
+import Carousel from "react-material-ui-carousel";
 
-// import image0 from "../utils/collection-1/image0-min.jpg";
-// import image1 from "../utils/collection-1/image1-min.jpg";
-// import image2 from "../utils/collection-1/image2-min.jpg";
-// import image3 from "../utils/collection-1/image3-min.jpg";
-// import image4 from "../utils/collection-1/image4-min.jpg";
-// import image5 from "../utils/collection-1/image5-min.jpg";
-// import image6 from "../utils/collection-1/image6-min.jpg";
-// import image7 from "../utils/collection-1/image7-min.jpg";
-// import image8 from "../utils/collection-1/image8-min.jpg";
-// import image9 from "../utils/collection-1/image9-min.jpg";
-// import image10 from "../utils/collection-1/image10-min.jpg";
-// import image11 from "../utils/collection-1/image11-min.jpg";
-// import image12 from "../utils/collection-1/image12-min.jpg";
-// import image13 from "../utils/collection-1/image13-min.jpg";
-// import image14 from "../utils/collection-1/image14-min.jpg";
-// import image15 from "../utils/collection-1/image15-min.jpg";
+import image0 from "../utils/collection-1/image0-min.jpg";
+import image1 from "../utils/collection-1/image1-min.jpg";
+import image2 from "../utils/collection-1/image2-min.jpg";
+import image3 from "../utils/collection-1/image3-min.jpg";
+import image4 from "../utils/collection-1/image4-min.jpg";
+import image5 from "../utils/collection-1/image5-min.jpg";
+import image6 from "../utils/collection-1/image6-min.jpg";
+import image7 from "../utils/collection-1/image7-min.jpg";
+import image8 from "../utils/collection-1/image8-min.jpg";
+import image9 from "../utils/collection-1/image9-min.jpg";
+import image10 from "../utils/collection-1/image10-min.jpg";
+import image11 from "../utils/collection-1/image11-min.jpg";
+import image12 from "../utils/collection-1/image12-min.jpg";
+import image13 from "../utils/collection-1/image13-min.jpg";
+import image14 from "../utils/collection-1/image14-min.jpg";
+import image15 from "../utils/collection-1/image15-min.jpg";
 
 function Home() {
   return (
@@ -25,7 +25,7 @@ function Home() {
       container
       style={{marginTop: 10}}
     >
-      {/* <Carousel
+      <Carousel
         indicatorIconButtonProps={{
           style: {
             display: "none",
@@ -50,61 +50,61 @@ function Home() {
             />
           );
         })}
-      </Carousel> */}
+      </Carousel>
     </Grid>
   );
 }
 
-// const itemData = [
-//   {
-//     img: image0,
-//   },
-//   {
-//     img: image1,
-//   },
-//   {
-//     img: image2,
-//   },
-//   {
-//     img: image3,
-//   },
-//   {
-//     img: image4,
-//   },
-//   {
-//     img: image5,
-//   },
-//   {
-//     img: image6,
-//   },
-//   {
-//     img: image7,
-//   },
-//   {
-//     img: image8,
-//   },
-//   {
-//     img: image9,
-//   },
-//   {
-//     img: image10,
-//   },
-//   {
-//     img: image11,
-//   },
-//   {
-//     img: image12,
-//   },
-//   {
-//     img: image13,
-//   },
-//   {
-//     img: image14,
-//   },
+const itemData = [
+  {
+    img: image0,
+  },
+  {
+    img: image1,
+  },
+  {
+    img: image2,
+  },
+  {
+    img: image3,
+  },
+  {
+    img: image4,
+  },
+  {
+    img: image5,
+  },
+  {
+    img: image6,
+  },
+  {
+    img: image7,
+  },
+  {
+    img: image8,
+  },
+  {
+    img: image9,
+  },
+  {
+    img: image10,
+  },
+  {
+    img: image11,
+  },
+  {
+    img: image12,
+  },
+  {
+    img: image13,
+  },
+  {
+    img: image14,
+  },
 
-//   {
-//     img: image15,
-//   },
-// ];
+  {
+    img: image15,
+  },
+];
 
 export default Home;
